4ea57c2f262ae429ff12d399bac17445b2659012
Dung Dao <daomanhdung@hotmail.com>
2020-05-20 16:51:23 +0700

    Add Provider Detail Form

--- src/pages/Providers/ProviderDetail.jsx
+++ src/pages/Providers/ProviderDetail.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
-import {Form, Input, Button} from 'antd';
+import {Form, Input, Button, Card} from 'antd';
 import AppLayout from "../../layouts/AppLayout";
 
 const ProviderDetail = () => {
     return (
         <AppLayout>
-            <Form>
-                <Form.Item
-                    label="ID"
-                    name="id"
+            <Card
+                title={"Chi Tiết Nhà Cung Cấp"}>
+                <Form
+                    labelCol={{span: 4, offset: 4}}
+                    labelAlign={"left"}
+                    wrapperCol={{span: 12}}
                 >
-                    <Input/>
-                </Form.Item>
-                <Form.Item
-                    label="Tên Nhà Cung Cấp"
-                    name="name"
-                >
-                    <Input/>
-                </Form.Item>
-                <Form.Item
-                    label="Số Điện Thoại"
-                    name="phone"
-                >
-                    <Input/>
-                </Form.Item>
-                <Form.Item
-                    label="Địa Chỉ"
-                    name="address"
-                >
-                    <Input/>
-                </Form.Item>
-            </Form>
+                    <Form.Item
+                        label="ID"
+                        name="id"
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Tên Nhà Cung Cấp"
+                        name="name"
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Số Điện Thoại"
+                        name="phone"
+                    >
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Địa Chỉ"
+                        name="address"
+                    >
+                        <Input/>
+                    </Form.Item>
+                </Form>
+            </Card>
         </AppLayout>
     );
 };
