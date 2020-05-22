96ec1d8441f1a9495349a10a020804ddeed8839b
Dung Dao <daomanhdung@hotmail.com>
2020-05-22 16:44:03 +0700

    Test git

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,117 +1,128 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import {Button, Popconfirm, Row, Space, Table} from "antd";
-import {InfoOutlined, EditOutlined, DeleteOutlined} from "@ant-design/icons";
-import {Col} from "antd";
+import { Button, Popconfirm, Row, Space, Table } from "antd";
+import { InfoOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Col } from "antd";
 import AdvancedSearchForm from "./SearchForm";
-import {useDispatch} from "react-redux";
-import {push} from 'connected-react-router';
-import {deleteProvider} from "../redux";
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
+import { deleteProvider } from "../redux";
 
 const DataTable = (props) => {
-    const dispatch = useDispatch();
-    return (
-        <React.Fragment>
-            <Row gutter={[16, 16]}>
-                <Col span={24}>
-                    <AdvancedSearchForm
-                        resourceName={props.resourceName}
-                        fields={props.columns.map((e) => {
-                            return {
-                                label: e.title,
-                                name: e.key,
-                                placeholder: `Nhập ${e.title.toLowerCase()}`,
-                            };
-                        })}
+  const dispatch = useDispatch();
+  return (
+    <React.Fragment>
+      <Row gutter={[16, 16]}>
+        <Col span={24}>
+          <AdvancedSearchForm
+            resourceName={props.resourceName}
+            fields={props.columns.map((e) => {
+              return {
+                label: e.title,
+                name: e.key,
+                placeholder: `Nhập ${e.title.toLowerCase()}`,
+              };
+            })}
+          />
+        </Col>
+        <Col span={24}>
+          <Table
+            dataSource={props.dataSource}
+            columns={[
+              {
+                title: "Thao Tác",
+                key: "action",
+                render: (record) => (
+                  <Space>
+                    <Button
+                      shape="circle"
+                      type="primary"
+                      icon={<InfoOutlined />}
+                      onClick={() => {
+                        dispatch(
+                          push(`/${props.resourceName}/${record.id}`, {
+                            action: "view",
+                            payload: record,
+                          })
+                        );
+                      }}
                     />
-                </Col>
-                <Col span={24}>
-                    <Table
-                        dataSource={props.dataSource}
-                        columns={[
-                            {
-                                title: "Thao Tác",
-                                key: "action",
-                                render: (record) => (
-                                    <Space>
-                                        <Button shape="circle" type="primary" icon={<InfoOutlined/>}
-                                                onClick={() => {
-                                                    dispatch(
-                                                        push(
-                                                            `/${props.resourceName}/${record.id}`,
-                                                            {action: "view", payload: record}
-                                                        )
-                                                    )
-                                                }}/>
-                                        <Button shape="circle" type="primary" icon={<EditOutlined/>}
-                                                onClick={() => {
-                                                    dispatch(
-                                                        push(
-                                                            `/${props.resourceName}/${record.id}`,
-                                                            {action: "edit", payload: record}
-                                                        )
-                                                    )
-                                                }}/>
-                                        <Popconfirm
-                                            title={"Bạn có chắc muốn xóa?"}
-                                            onConfirm={() => {
-                                                dispatch(props.onDelete(record));
-                                            }}>
-                                            <Button shape="circle" type="danger" icon={<DeleteOutlined/>}/>
-                                        </Popconfirm>
-                                    </Space>
-                                ),
-                            },
-                            ...props.columns
-                        ]}
-                        style={{flexFlow: 1}}
-                        pagination={
-                            {
-                                defaultPageSize: 5,
-                                showSizeChanger: true,
-                                pageSizeOptions: ['5', '10', '20']
-                            }
-                        }
+                    <Button
+                      shape="circle"
+                      type="primary"
+                      icon={<EditOutlined />}
+                      onClick={() => {
+                        dispatch(
+                          push(`/${props.resourceName}/${record.id}`, {
+                            action: "edit",
+                            payload: record,
+                          })
+                        );
+                      }}
                     />
-                </Col>
-            </Row>
-        </React.Fragment>
-    );
+                    <Popconfirm
+                      title={"Bạn có chắc muốn xóa?"}
+                      onConfirm={() => {
+                        dispatch(props.onDelete(record));
+                      }}
+                    >
+                      <Button
+                        shape="circle"
+                        type="danger"
+                        icon={<DeleteOutlined />}
+                      />
+                    </Popconfirm>
+                  </Space>
+                ),
+              },
+              ...props.columns,
+            ]}
+            style={{ flexFlow: 1 }}
+            pagination={{
+              defaultPageSize: 5,
+              showSizeChanger: true,
+              pageSizeOptions: ["5", "10", "20"],
+            }}
+          />
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 };
 
 DataTable.propTypes = {
-    resourceName: PropTypes.string.isRequired,
-    columns: PropTypes.arrayOf(
-        PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            key: PropTypes.string.isRequired,
-            dataIndex: PropTypes.string,
-            sorter: PropTypes.func //sort function return a - b
-        })
-    ).isRequired,
-    dataSource: PropTypes.array.isRequired,
-    onCreate: PropTypes.func.isRequired,
-    onUpdate: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    onView: PropTypes.func.isRequired,
+  resourceName: PropTypes.string.isRequired,
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      key: PropTypes.string.isRequired,
+      dataIndex: PropTypes.string,
+      sorter: PropTypes.func, //sort function return a - b
+    })
+  ).isRequired,
+  dataSource: PropTypes.array.isRequired,
+  onCreate: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onView: PropTypes.func.isRequired,
 };
 
 DataTable.defaultProps = {
-    columns: [],
-    dataSource: [],
-    onCreate: function () {
-        console.log('create')
-    },
-    onUpdate: function () {
-        console.log('update')
-    },
-    onDelete: function () {
-        console.log('delete')
-    },
-    onView: function () {
-        console.log('view')
-    }
+  columns: [],
+  dataSource: [],
+  onCreate: function () {
+    console.log("create");
+  },
+  onUpdate: function () {
+    console.log("update");
+  },
+  onDelete: function () {
+    console.log("delete");
+  },
+  onView: function () {
+    console.log("view");
+  },
 };
 
 export default DataTable;
