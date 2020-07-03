a9196686efc6c9837ec4aab9f7284e097c1043e6
Dung Dao <daomanhdung@hotmail.com>
2020-07-03 13:53:04 +0700

    Small fix login page

--- src/layouts/BlankLayout.jsx
+++ src/layouts/BlankLayout.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./BlankLayout.css";
-import { Layout } from "antd";
-const { Footer, Content } = Layout;
+import {Layout} from "antd";
+
+const {Footer, Content} = Layout;
 
 class BlankLayout extends Component {
-  render() {
-    return (
-      <Layout className="main">
-        <Content className="content">{this.props.children}</Content>
-        <Footer className="footer">Footer</Footer>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout style={{minHeight: "100vh"}}>
+                <Content>{this.props.children}</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        );
+    }
 }
 
 export default BlankLayout;
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,84 +1,86 @@
 import React from "react";
 import "./SignIn.css";
-import { Redirect } from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import BlankLayout from "../../layouts/BlankLayout";
-import { Form, Input, Button, Checkbox } from "antd";
-import { Row } from "antd";
-import { login, userSelector } from "../../redux";
-import { useDispatch, useSelector } from "react-redux";
+import {login, userSelector} from "../../redux";
+import {useDispatch, useSelector} from "react-redux";
+import {Button, Checkbox, Col, Form, Input, Row} from 'antd';
 
 const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+    labelAlign: "left"
 };
 
 const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 16,
-  },
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
 };
 
 const SignIn = () => {
-  const dispatch = useDispatch();
-  const user = useSelector(userSelector);
-  if (user && user.isLogged) return <Redirect to="/"></Redirect>;
-  return (
-    <BlankLayout>
-      <Row align="middle" justify="center" style={{ padding: "4em" }}>
-        <Form
-          {...layout}
-          name="basic"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={(values) => {
-            dispatch(login(values));
-          }}
-        >
-          <Form.Item
-            label="Tên đăng nhập"
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "Nhập tên đăng nhập!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+    const dispatch = useDispatch();
+    const user = useSelector(userSelector);
+    if (user && user.isLogged) return <Redirect to="/"></Redirect>;
+    return (
+        <BlankLayout>
+            <Row align="middle" justify="center">
+                <Col md={8} xs={24}>
+                    <Form
+                        {...layout}
+                        style={{marginTop: "8em"}}
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={(values) => {
+                            dispatch(login(values));
+                        }}
+                    >
+                        <Form.Item
+                            label="Tên đăng nhập"
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Nhập tên đăng nhập!",
+                                },
+                            ]}
+                        >
+                            <Input/>
+                        </Form.Item>
 
-          <Form.Item
-            label="Mật khẩu"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Nhập mật khẩu!",
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
+                        <Form.Item
+                            label="Mật khẩu"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Nhập mật khẩu!",
+                                },
+                            ]}
+                        >
+                            <Input.Password/>
+                        </Form.Item>
+                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                            <Checkbox>Lưu mật khẩu</Checkbox>
+                        </Form.Item>
 
-          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-            <Checkbox>Lưu mật khẩu</Checkbox>
-          </Form.Item>
-
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit" loading={user.isFetching}>
-              Đăng Nhập
-            </Button>
-          </Form.Item>
-        </Form>
-      </Row>
-    </BlankLayout>
-  );
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit" loading={user.isFetching}>
+                                Đăng Nhập
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </BlankLayout>
+    );
 };
 
 export default SignIn;
